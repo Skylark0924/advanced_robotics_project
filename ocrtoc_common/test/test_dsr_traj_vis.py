9cabe22bcd1d4aac155c0662e6a247f602541302
Skylar <skylark0924@gmail.com>
2021-11-22 14:35:54 +0800

    Solve the bug of Non-ASCII character '\xe8'

--- ocrtoc_common/test/test_dsr_traj_vis.py
+++ ocrtoc_common/test/test_dsr_traj_vis.py
@@ -1,6 +1,4 @@
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 import pdb
 
 import rospy
@@ -9,25 +7,19 @@ from geometry_msgs.msg import PoseStamped, Quaternion
 import tf
 import math
 
-# 起始运动状态
 x, y, z, th = 0.125, 0.125, 0.5, 0
 
 
 def DataUpdating(path_pub, path_record):
-    """
-    数据更新函数
-    """
     global x, y, z, th
 
-    # 时间戳
     current_time = rospy.Time.now()
 
-    # 配置运动
     dt = 0.005
     vx = 0.5
     vy = 0
     vz = 0.5
-    vth = 7
+    vth = 9
     # delta_x = (vx * math.cos(th) - vy * math.sin(th)) * dt
     # delta_y = (vx * math.sin(th) + vy * math.cos(th)) * dt
     delta_y = (vy * math.cos(th) - vz * math.sin(th)) * dt
@@ -39,10 +31,8 @@ def DataUpdating(path_pub, path_record):
     z += delta_z
     th += delta_th
 
-    # 四元素转换
     quat = tf.transformations.quaternion_from_euler(0, 0, th)
 
-    # 配置姿态
     pose = PoseStamped()
     pose.header.stamp = current_time
     pose.header.frame_id = "world"
@@ -54,48 +44,33 @@ def DataUpdating(path_pub, path_record):
     pose.pose.orientation.z = quat[2]
     pose.pose.orientation.w = quat[3]
 
-    # 配置路径
     path_record.header.stamp = current_time
     path_record.header.frame_id = "world"
     path_record.poses.append(pose)
 
-    # 路径数量限制
     if len(path_record.poses) > 1000:
         path_record.poses.pop(0)
 
-    # 发布路径
     path_pub.publish(path_record)
 
 
 def node():
-    """
-    节点启动函数
-    """
     try:
+        rospy.init_node('PathRecord')
 
-        # 初始化节点path
-        rospy.init_node('PathRecord', anonymous=True )
-
-        # 定义发布器 path_pub 发布 trajectory
         path_pub = rospy.Publisher('dsr_traj', Path, queue_size=50)
 
-        # 初始化循环频率
         rate = rospy.Rate(50)
 
-        # 定义路径记录
         path_record = Path()
 
-        # 发布tf
         # br = tf.TransformBroadcaster()
         # br.sendTransform((0.0, 0.0, 0.0), (0.0, 0.0, 0.0, 1.0),
         #                  rospy.Time.now(), "odom", "world")
 
-        # 在程序没退出的情况下
         while not rospy.is_shutdown():
-            # 数据更新函数
             DataUpdating(path_pub, path_record)
 
-            # 休眠
             rate.sleep()
 
     except rospy.ROSInterruptException:
