9b2f981e4629cafc213fea823c872ee2bf30eaa8
Skylark <skylark0924@gmail.com>
2021-12-10 11:07:44 +0800

    Fix a minor bug about the initial position of the desired traj

--- ocrtoc_common/test/trajectory_tracking.py
+++ ocrtoc_common/test/trajectory_tracking.py
@@ -10,26 +10,27 @@ class DesiredTraj:
     def __init__(self, timespan, delta_t, r=None, num=None, shift=None):
         self.x, self.y, self.z, self.theta, self.phi, self.psi = np.zeros(timespan), np.zeros(timespan), np.zeros(
             timespan), np.zeros(timespan), np.zeros(timespan), np.zeros(timespan)
-        self.xt, self.yt, self.zt, self.thetat, self.phit, self.psit = 0, 0, 0, 0, 0, 0
+        self.xt, self.yt, self.zt, self.thetat, self.phit, self.psit = 0.125, 0.125, 0.5, 0, 0, 0
 
         self.r = r
         self.num = num
         self.shift = shift
         self.timespan = timespan
         self.delta_t = delta_t
+        self.theta_circle = 0
 
     def get_instant_pose_circle(self):
         vy = 0
         vz = 0.5
         vth = 7
-        delta_y = (vy * math.cos(self.thetat) - vz * math.sin(self.thetat)) * self.delta_t
-        delta_z = (vy * math.sin(self.thetat) + vz * math.cos(self.thetat)) * self.delta_t
+        delta_y = (vy * math.cos(self.theta_circle) - vz * math.sin(self.theta_circle)) * self.delta_t
+        delta_z = (vy * math.sin(self.theta_circle) + vz * math.cos(self.theta_circle)) * self.delta_t
         delta_th = vth * self.delta_t
         self.yt += delta_y
         self.zt += delta_z
-        self.thetat += delta_th
+        self.theta_circle += delta_th
 
-        traj_t = np.array([self.xt, self.yt, self.zt, 0, 0, 0])
+        traj_t = np.array([self.xt, self.yt, self.zt, self.thetat, self.phit, self.psit])
         trajd_t = np.array([0, 0, 0, 0, 0, 0])
         return traj_t, trajd_t
 
